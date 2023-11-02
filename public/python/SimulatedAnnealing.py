a6f273a2f4aa482ea9bd2eaa16b858cef5937608
Davi Lemes <ndlemes7@outlook.com>
2023-11-02 20:15:46 -0300

    new update

--- public/python/SimulatedAnnealing.py
+++ public/python/SimulatedAnnealing.py
@@ -7,8 +7,9 @@ from random import sample
 from readXls import ReadData, SingleSolution
 
 class SimulatedAnnealing:
-    def __init__(self, data, min_temp, max_temp, cooling_rate=0.999):
-        self.data = data
+    def __init__(self, clean_data, min_temp, max_temp, cooling_rate=0.999):
+        self.clean_data = clean_data
+        self.data = deepcopy(clean_data)
         #self.original = deepcopy(grade)
         self.min_temp = min_temp
         self.max_temp = max_temp
@@ -26,11 +27,24 @@ class SimulatedAnnealing:
             counter += 1
 
             if counter % 100 == 0:
+                # print('---------------------------------------------------------------------------')
+                # print('---------------------------------------------------------------------------')
+                # print('---------------------------------------------------------------------------')
+                # print('---------------------------------------------------------------------------')
                 print('Iteracao #%s - quantidade de conflitos: %s' % (counter, self.best_state.fitness()))
+                #
+                # for fase in self.best_state.grade:
+                #     for index, dia in enumerate(fase):
+                #         for slot in dia:
+                #             if (slot is None):
+                #                 print("(-)")
+                #             else:
+                #                 print(slot.nome_disciplina + " " + slot.professor.nome_chave)
 
-            new_state = self.actual_state
-            # new_state = SingleSolution(data)
-            # new_state.generate_solution()
+            #new_state = self.actual_state
+            new_data = deepcopy(clean_data)
+            new_state = SingleSolution(new_data)
+            new_state.generate_solution()
 
             actual_energy = self.actual_state.fitness()
             new_energy = new_state.fitness()
@@ -44,36 +58,44 @@ class SimulatedAnnealing:
             temp = temp * self.cooling_rate
 
         for fase in self.best_state.grade:
+            i = 0
             for index, dia in enumerate(fase):
+               i = 0
                for slot in dia:
                    if(slot is None):
                        print("(-)")
                    else:
-                    print(slot.nome_disciplina + " " +slot.professor.nome_chave)
-            print('-----------------------------------------')
+                    print(slot.nome_disciplina + " " + slot.professor.nome_chave)
+            print('\n')
+            print('\n')
+            print('|||||||||||||||||||||||||||||||||||||||||||')
+            print('\n')
+            print('\n')
+
 
         print('Solucao: \n%s' % self.best_state)
+        print('conflitos: ' + str(self.best_state.fitness()))
 
     # swapa dias discilpna de lugar aleatoriamente
-    def generate_next_state(self, actual_state):
-        new_state = SingleSolution(data)
-        new_state.generate_solution()
-        return new_state
+    # def generate_next_state(self, actual_state):
+    #     new_state = SingleSolution(data)
+    #     new_state.generate_solution()
+    #     return new_state
 
     @staticmethod
     def accept_prob(actual_energy, next_energy, temp):
-
         if next_energy < actual_energy:
             return 1
 
+        #return 0
         return np.exp((actual_energy - next_energy) / temp)
-
 
 if __name__ == '__main__':
     data = ReadData()
     data.read_professores()
     data.read_disciplinas()
-    algorithm = SimulatedAnnealing(data, 1, 100)
+    clean_data = deepcopy(data)
+    algorithm = SimulatedAnnealing(clean_data, 1, 100)
     algorithm.run()
 
     # for index, dia in enumerate(matriz):
--- public/python/readXls.py
+++ public/python/readXls.py
@@ -1,11 +1,17 @@
+from copy import deepcopy
+from random import shuffle
+
 import os
 import sys
+
 import pandas as pd
 import numpy as np
 
-from constraints import ConstraintHard, ConstraintSoft, DiasNaoMinistradasHard, DiasNaoMinistradasSoft, DeveSerNoiteCheiaHard, DeveSerNoiteCheiaSoft
+from constraints import ConstraintHard, ConstraintSoft, DiasNaoMinistradasHard, DiasNaoMinistradasSoft, \
+    DeveSerNoiteCheiaHard, DeveSerNoiteCheiaSoft
 from professor import Professor
 from disciplina import Disciplina
+
 
 class ReadData:
     def __init__(self):
@@ -26,19 +32,18 @@ class ReadData:
         # Pega os dados com um dicionario
         data = df.to_dict(orient='records')
 
-
         # Pega o cabecalho na primeira linha
         header = data[0]
-        data = data[1:] #Ignorando a linha do cabecalho para os dados
+        data = data[1:]  # Ignorando a linha do cabecalho para os dados
 
-        #Remove as colunas sem info do cabecalho.
+        # Remove as colunas sem info do cabecalho.
         header = [coluna for coluna in header if pd.notna(coluna)]
 
         professores = []
 
         i = 1
         for linha in data:
-            hard =  DiasNaoMinistradasHard(False, linha[3])
+            hard = DiasNaoMinistradasHard(False, linha[3])
             soft = DiasNaoMinistradasSoft(False, linha[5], linha[6])
             professor = Professor(i, linha[0], linha[1], hard, soft)
             self.cacheProfessores[linha[0]] = professor
@@ -47,17 +52,16 @@ class ReadData:
 
         return professores
 
-    #print(cacheProfessores)
+    # print(cacheProfessores)
 
     # for p in professores:
     #     print(p.nome_chave + ' ' + p.nome_completo + ' ' + str(p.diasNaoMinistradosHard.getDiasNaoMinistradas()) + ' ' + str(p.diasNaoMinistradosSoft.getDiasNaoMinistradas()))
 
     #####################################################################
 
-
     def read_disciplinas(self):
 
-        df = pd.read_excel(os.getcwd()+ "/python/planilhas/" + sys.argv[2], header=None, usecols=[0,1,2,3,4,6,7,9,10,11,12]) ##descobrir uma mehlor forma de nao pegar as coisas vazias
+        df = pd.read_excel(os.getcwd()+ "/python/planilhas/" + sys.argv[2], header=None, usecols=[0,1,2,3,4,6,7,9,10,11,12])
         df = df.replace(np.nan, None)
 
         data = df.to_dict(orient='records')
@@ -71,15 +75,17 @@ class ReadData:
         i = 1
         for linha in data:
             hard_constraints = [DiasNaoMinistradasHard(False, linha[6]), DeveSerNoiteCheiaHard(False, linha[7])]
-            soft_constraints = [DiasNaoMinistradasSoft(False, linha[9],linha[10]), DeveSerNoiteCheiaSoft(False, linha[11], linha[12])]
-            disciplina = Disciplina(i, self.cacheProfessores[linha[0]], linha[1], linha[2], linha[3], linha[4], hard_constraints, soft_constraints)  ## como la em cima ta setado as cols aqui tem q fazer tbm, bem meme
+            soft_constraints = [DiasNaoMinistradasSoft(False, linha[9], linha[10]),
+                                DeveSerNoiteCheiaSoft(False, linha[11], linha[12])]
+            disciplina = Disciplina(i, self.cacheProfessores[linha[0]], linha[1], linha[2], linha[3], linha[4],
+                                    hard_constraints,
+                                    soft_constraints)  ## como la em cima ta setado as cols aqui tem q fazer tbm, bem meme
 
             ##ADICIONA CONSTRAINT DA DSISCIPLNA NO PRFOESSOR QUE LECIONA
             prof = self.cacheProfessores[linha[0]]
             prof.addHardConstraints(hard_constraints)
 
-
-            if(disciplina.fase == '1'):
+            if (disciplina.fase == '1'):
                 self.primeiraFase.append(disciplina)
             if (disciplina.fase == '2'):
                 self.segundaFase.append(disciplina)
@@ -99,6 +105,7 @@ class ReadData:
             self.disciplinas.append(disciplina)
             i += 1
 
+
 # for disciplina in primeiraFase:
 #     print(f'ID: {disciplina.id}, ' \
 #                        f'Nome-Chave Professor: {disciplina.professor}, ' \
@@ -115,7 +122,7 @@ class ReadData:
 #                        f'Peso 2: {disciplina.soft_constraints[1].getPeso()}')
 #     print("----------------------------------------------------------------")
 
-#ESSA FUNCAO NAO ADICIONA DICIPLINAS QUE VIOALM RESTRICOES
+# ESSA FUNCAO NAO ADICIONA DICIPLINAS QUE VIOALM RESTRICOES
 def gerarSolucaoParical(matriz, disciplinas):
     podeSabado = []
     for indice, disciplina in enumerate(disciplinas):
@@ -165,24 +172,27 @@ def gerarSolucaoParical(matriz, disciplinas):
 
     conflitos = 0;
     for index, dia in enumerate(matriz):
-       for slot in dia:
-           if(slot is None):
-               if index != 5:
-                   conflitos += 1
-               print("(-)")
-           else:
-            print(slot.nome_disciplina + " " +slot.professor.nome_chave)
-       print("------------------------------------------------------------------------")
+        for slot in dia:
+            if (slot is None):
+                if index != 5:
+                    conflitos += 1
+                print("(-)")
+            else:
+                print(slot.nome_disciplina + " " + slot.professor.nome_chave)
+        print("------------------------------------------------------------------------")
 
     return conflitos
 
-#ESSA FUNCAO GERA A GRADE POR COMPLETO, ADICIONANDO AS DISCIPLINAS QUE VIOLAM RESTRICOES
+
+# ESSA FUNCAO GERA A GRADE POR COMPLETO, ADICIONANDO AS DISCIPLINAS QUE VIOLAM RESTRICOES
 def gerar(matriz, disciplinas):
-    np.random.shuffle(disciplinas)
+    #np.random.shuffle(disciplinas)
 
     podeSabado = []
 
     disciplinasNaoAlocadas = []
+
+    disciplinasNaoAlocadasSabado = []
 
     for indice, disciplina in enumerate(disciplinas):
         if disciplina.aula_sabado == 'S':
@@ -194,9 +204,11 @@ def gerar(matriz, disciplinas):
         j = 0
         qntdAulas = int(disciplina.creditos_disciplina / 2)
         alocadas = 0
+        alocados = []
         while not disciplina.alocada:
             if matriz[i][j] is None and disciplina.professor.horariosAlocados[i][j] is None:
                 matriz[i][j] = disciplina
+                alocados.append([i, j])
                 alocadas += 1
                 disciplina.professor.horariosAlocados[i][j] = disciplina
                 if qntdAulas == alocadas:
@@ -205,28 +217,11 @@ def gerar(matriz, disciplinas):
             if j == 2:  # Avance para a próxima coluna
                 j = 0
                 i += 1  # Avance para a próxima linha
-            if i == 5:  # Verifique se todas as linhas foram preenchidas
-                disciplinasNaoAlocadas.append(disciplina)
-                break
-
-    for index, disciplina in enumerate(podeSabado):
-        i = 0
-        j = 0
-        qntdAulas = int(disciplina.creditos_disciplina / 2)
-        alocadas = 0
-        while not disciplina.alocada:
-            if matriz[i][j] is None and disciplina.professor.horariosAlocados[i][j] is None:
-                matriz[i][j] = disciplina
-                alocadas += 1
-                disciplina.professor.horariosAlocados[i][j] = disciplina
-                if qntdAulas == alocadas:
-                    disciplina.alocada = True
-            j += 1
-            if j == 2:  # Avance para a próxima coluna
-                j = 0
-                i += 1  # Avance para a próxima linha
-            if i == 6:  # Verifique se todas as linhas foram preenchidas
-                disciplinasNaoAlocadas.append(disciplina)
+            if i == 5:
+                if disciplina.alocada is False:
+                    for ij in alocados:
+                        matriz[ij[0]][1] = None
+                    disciplinasNaoAlocadas.append(disciplina)
                 break
 
     conflitos = 0;
@@ -239,6 +234,7 @@ def gerar(matriz, disciplinas):
             if matriz[i][j] is None:
                 matriz[i][j] = disciplina
                 alocadas += 1
+                disciplina.nome_disciplina = disciplina.nome_disciplina + " *********"
                 conflitos += 1
                 if qntdAulas == alocadas:
                     disciplina.alocada = True
@@ -246,9 +242,59 @@ def gerar(matriz, disciplinas):
             if j == 2:  # Avance para a próxima coluna
                 j = 0
                 i += 1  # Avance para a próxima linha
-            if i == 6:  # Verifique se todas as linhas foram preenchidas
+            if i == 5:  # Verifique se todas as linhas foram preenchidas
                 break
+
+
+    for index, disciplina in enumerate(podeSabado):
+        i = 0
+        j = 0
+        qntdAulas = int(disciplina.creditos_disciplina / 2)
+        alocadas = 0
+        alocados = []
+        while not disciplina.alocada:
+            if matriz[i][j] is None and disciplina.professor.horariosAlocados[i][j] is None:
+                matriz[i][j] = disciplina
+                alocados.append([i, j])
+                alocadas += 1
+                disciplina.professor.horariosAlocados[i][j] = disciplina
+                if qntdAulas == alocadas:
+                    disciplina.alocada = True
+            j += 1
+            if j == 2:  # Avance para a próxima coluna
+                j = 0
+                i += 1  # Avance para a próxima linha
+            if i == 6:  # Verifique se todas as linhas foram preenchidas
+                if disciplina.alocada is False:
+                    for ij in alocados:
+                        matriz[ij[0]][1] = None
+                    disciplinasNaoAlocadasSabado.append(disciplina)
+                break
+
+
+    for index, disciplina in enumerate(disciplinasNaoAlocadasSabado):
+        i = 0
+        j = 0
+        qntdAulas = int(disciplina.creditos_disciplina / 2)
+        alocadas = 0
+        while not disciplina.alocada:
+            if matriz[i][j] is None:
+                matriz[i][j] = disciplina
+                alocadas += 1
+                disciplina.nome_disciplina = disciplina.nome_disciplina + " *********"
+                conflitos += 1
+                if qntdAulas == alocadas:
+                    disciplina.alocada = True
+            j += 1
+            if j == 2:  # Avance para a próxima coluna
+                j = 0
+                i += 1  # Avance para a próxima linha
+            if i == 5:  # Verifique se todas as linhas foram preenchidas
+                break
+
     return conflitos
+
+
 
 
 #
@@ -324,47 +370,63 @@ def gerar(matriz, disciplinas):
 # print("TOTAL DE CONFLITOS DA GRADE:" + str(totalConflitos))
 
 class SingleSolution:
-    def __init__(self, data):
+    def __init__(self, original_data):
         self.grade = None
         self.totalConflitos = None
-        self.data = data;
+        self.data = deepcopy(original_data)
 
     def generate_solution(self):
         self.totalConflitos = 0
 
+        fases = [1, 2, 3, 4, 5, 6, 7, 8]
+
+        shuffle(fases)
+
         matriz1Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf1 = gerar(matriz1Fase, self.data.primeiraFase)
-        self.totalConflitos += conf1
 
         matriz2Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf2 = gerar(matriz2Fase, self.data.segundaFase)
-        self.totalConflitos += conf2
 
         matriz3Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf3 = gerar(matriz3Fase, self.data.terceiraFase)
-        self.totalConflitos += conf3
 
         matriz4Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf4 = gerar(matriz4Fase, self.data.quartaFase)
-        self.totalConflitos += conf4
 
         matriz5Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf5 = gerar(matriz5Fase, self.data.quintaFase)
-        self.totalConflitos += conf5
 
         matriz6Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf6 = gerar(matriz6Fase, self.data.sextaFase)
-        self.totalConflitos = + conf6
 
         matriz7Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf7 = gerar(matriz7Fase, self.data.setimaFase)
-        self.totalConflitos += conf7
 
         matriz8Fase = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
-        conf8 = gerar(matriz8Fase, self.data.oitavaFase)
-        self.totalConflitos += conf8
 
-        self.grade = [matriz1Fase,matriz2Fase,matriz3Fase,matriz4Fase,matriz5Fase,matriz6Fase, matriz7Fase, matriz8Fase] #grade de todas as fasese
+        # Loop para gerar as matrizes de acordo com a ordem aleatória.
+        for fase in fases:
+            if fase == 1:
+                shuffle(self.data.primeiraFase)
+                self.totalConflitos += gerar(matriz1Fase, self.data.primeiraFase)
+            elif fase == 2:
+                shuffle(self.data.segundaFase)
+                self.totalConflitos += gerar(matriz2Fase, self.data.segundaFase)
+            elif fase == 3:
+                shuffle(self.data.terceiraFase)
+                self.totalConflitos += gerar(matriz3Fase, self.data.terceiraFase)
+            elif fase == 4:
+                shuffle(self.data.quartaFase)
+                self.totalConflitos += gerar(matriz4Fase, self.data.quartaFase)
+            elif fase == 5:
+                shuffle(self.data.quintaFase)
+                self.totalConflitos += gerar(matriz5Fase, self.data.quintaFase)
+            elif fase == 6:
+                shuffle(self.data.sextaFase)
+                self.totalConflitos += gerar(matriz6Fase, self.data.sextaFase)
+            elif fase == 7:
+                shuffle(self.data.setimaFase)
+                self.totalConflitos += gerar(matriz7Fase, self.data.setimaFase)
+            elif fase == 8:
+                shuffle(self.data.oitavaFase)
+                self.totalConflitos += gerar(matriz8Fase, self.data.oitavaFase)
+
+        self.grade = [matriz1Fase, matriz2Fase, matriz3Fase, matriz4Fase,
+                      matriz5Fase, matriz6Fase, matriz7Fase,matriz8Fase]  # grade de todas as fasese em ordem
 
     def fitness(self):
         return self.totalConflitos
